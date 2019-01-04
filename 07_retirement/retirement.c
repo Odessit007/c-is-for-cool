34073b73d072935e66ff7f302a3e49f143b89690
Odessit007 <igor.vustyanyuk@gmail.com>
2019-01-04 20:59:45 +0200

    7

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 
 
-typedef struct _retire_info {
+struct _retire_info {
     int months;
     double contribution;
     double rate_of_return;
-} retire_info;
+};
+
+typedef struct _retire_info retire_info;
 
 double collect(int startAge, double initial, retire_info info) {
     double balance = initial;
